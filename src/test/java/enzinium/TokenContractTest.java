c98d30fd2d849ddf42ad936a0c6447c9ece41cd3
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-02-21 11:19:32 +0100

    Test comprobar que el mapa solo almacena una vez

--- src/test/java/enzinium/TokenContractTest.java
+++ src/test/java/enzinium/TokenContractTest.java
@@ -13,7 +13,7 @@ public class TokenContractTest {
         TokenContract rick = new TokenContract(address);
         assertNotNull(rick);}
 
-
+    @Test
     public void contractTest(){
         Address address = new Address();
         TokenContract ricknillos = new TokenContract(address);
@@ -23,5 +23,16 @@ public class TokenContractTest {
         assertEquals("Ricknillos", ricknillos.name());
         assertEquals("RNILL", ricknillos.symbol());
         assertEquals(100d, ricknillos.totalSupply(), 0);
+    }
+
+    @Test
+    public void mapTest{
+        Address rick = new Address();
+        TokenContract ricknillos = new TokenContract(rick);
+        ricknillos.setTotalSupply(100);
+        ricknillos.addOwner(rick.getPK(), ricknillos.totalSupply());
+        ricknillos.setTotalSupply(500);
+        ricknillos.addOwner(rick.getPK(), ricknillos.totalSupply());
+        assertEquals(100, ricknillos.getBalances().get(rick.getPK()), 0);
     }
 }
