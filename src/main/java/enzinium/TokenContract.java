c0388b33847f1fd17615c21ea8e771c0eaa8a076
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-02-21 11:12:30 +0100

    Creada la clase y sus metodos que pasan los casos test

---
+++ src/main/java/enzinium/TokenContract.java
@@ -0,0 +1,69 @@
+package enzinium;
+
+import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TokenContract {
+
+        private PublicKey owner = null;
+        private String name = null;
+        private double totalSUpply = 0d;
+        private String symbol = null;
+        private Map<PublicKey, Double> balances = new HashMap<>();
+
+        //CONSTRUCTOR
+        public TokenContract (Address address){
+            this.owner = address.getPK();
+        }
+
+
+        //SETTERS
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setTotalSupply(double totalSUpply) {
+            this.totalSUpply = totalSUpply;
+        }
+
+        public void setSymbol(String symbol) {
+            this.symbol = symbol;
+        }
+
+        public void setBalances(Map<PublicKey, Double> balances) {
+            this.balances = balances;
+        }
+
+
+        //GETTERS
+
+
+        public String name() {
+            return this.name;
+        }
+
+        public double totalSupply() {
+            return this.totalSUpply;
+        }
+
+        public String symbol() {
+            return this.symbol;
+        }
+
+        public Map<PublicKey, Double> getBalances() {
+            return this.balances;
+        }
+
+
+        //LÓGICA
+
+        @Override
+        public String toString(){
+            return "\n" + "name = " + name() +"\n"+
+                    "symbol = " + symbol() + "\n" +
+                    "totalSupply = " + totalSupply() + "\n"+
+                    "owner Pk = " + owner.hashCode();
+        }
+}
