13496445f413de882eb25fe82fd27d43750f80cc
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-02-21 11:13:03 +0100

    Pasa los casos test

--- src/test/java/enzinium/TokenContractTest.java
+++ src/test/java/enzinium/TokenContractTest.java
@@ -10,18 +10,18 @@ public class TokenContractTest {
     @Test
     public void clssTest(){
         Address address = new Address();
-        TokenContract rick = new TokenContract();
+        TokenContract rick = new TokenContract(address);
         assertNotNull(rick);}
 
 
     public void contractTest(){
         Address address = new Address();
-        TokenContract ricknillos = new TokenContract();
+        TokenContract ricknillos = new TokenContract(address);
         ricknillos.setName("Ricknillos");
         ricknillos.setSymbol("RNILL");
         ricknillos.setTotalSupply(100);
         assertEquals("Ricknillos", ricknillos.name());
         assertEquals("RNILL", ricknillos.symbol());
-        assertEquals(100d, ricknillos.totalSuplly(), 0);
+        assertEquals(100d, ricknillos.totalSupply(), 0);
     }
 }
