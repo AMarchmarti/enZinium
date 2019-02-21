d9c3ec1f3ed7d50db1347d3df447ffeb83e86a00
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-02-21 10:40:35 +0100

    App y Gensig puestas en seguimiento aplicados casos test para Address

---
+++ src/test/java/enzinium/AddressTest.java
@@ -0,0 +1,26 @@
+package enzinium;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.sun.jndi.cosnaming.IiopUrl;
+import org.junit.Test;
+
+public class AddressTest
+{
+
+    @Test
+    public void clssTest(){
+        Address address = new Address();
+        assertNotNull(address);
+}
+
+    @Test
+    public void generateKeyTest(){
+        Address address = new Address();
+        address.generateKeyPair();
+        assertNotNull(address.getPK());
+        assertNotNull(address.getSk);
+    }
+}
+
