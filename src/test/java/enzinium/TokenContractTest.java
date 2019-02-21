38c37e55e942670d18b53fcd19b7f76c71cc066b
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-02-21 11:03:00 +0100

    Nuevos casos Test para la clase TokenContract

--- src/test/java/enzinium/TokenContractTest.java
+++ src/test/java/enzinium/TokenContractTest.java
@@ -1,4 +1,27 @@
 package enzinium;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class TokenContractTest {
+
+    @Test
+    public void clssTest(){
+        Address address = new Address();
+        TokenContract rick = new TokenContract();
+        assertNotNull(rick);}
+
+
+    public void contractTest(){
+        Address address = new Address();
+        TokenContract ricknillos = new TokenContract();
+        ricknillos.setName("Ricknillos");
+        ricknillos.setSymbol("RNILL");
+        ricknillos.setTotalSupply(100);
+        assertEquals("Ricknillos", ricknillos.name());
+        assertEquals("RNILL", ricknillos.symbol());
+        assertEquals(100d, ricknillos.totalSuplly(), 0);
+    }
 }
