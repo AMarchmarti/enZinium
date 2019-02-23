9ed3820b68fb25b3353fe35d6043e5895c833121
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-02-23 14:11:11 +0100

    Final de la clase, pasa todos los casos test

--- src/main/java/enzinium/TokenContract.java
+++ src/main/java/enzinium/TokenContract.java
@@ -12,6 +12,7 @@ public class TokenContract {
         private double totalSUpply = 0d;
         private String symbol = null;
         private Map<PublicKey, Double> balances = new HashMap<>();
+        private double COSTE = 5.0;
 
         //CONSTRUCTOR
         public TokenContract (Address address){
@@ -55,6 +56,8 @@ public class TokenContract {
             return this.balances;
         }
 
+        public PublicKey owner(){ return this.owner;}
+
 
         //LÓGICA
 
@@ -63,7 +66,7 @@ public class TokenContract {
             return "\n" + "name = " + name() +"\n"+
                     "symbol = " + symbol() + "\n" +
                     "totalSupply = " + totalSupply() + "\n"+
-                    "owner Pk = " + owner.hashCode();
+                    "owner Pk = " + owner().hashCode();
         }
 
 
@@ -95,13 +98,14 @@ public class TokenContract {
 
 
         public void require(Double cantidad){
-            if ((cantidad > getBalances().get(owner)))
+            if ((cantidad > getBalances().get(owner())))
                 throw new AssertionError();
         }
 
 
         public void changeTokens(PublicKey key, Double cantidad){
-            getBalances().replace(owner, totalSupply() - cantidad);
+            setTotalSupply(totalSupply() - cantidad);
+            getBalances().replace(owner(), totalSupply());
             if (!getBalances().containsKey(key)){
                 getBalances().put(key, cantidad);
             }else{
@@ -132,7 +136,7 @@ public class TokenContract {
 
         public void owners(){
             for (PublicKey pKey : getBalances().keySet()){
-                if (!pKey.equals(owner)){
+                if (!pKey.equals(owner())){
                     System.out.println("Owners = " + pKey.hashCode() + " " + getBalances().get(pKey) + " " + symbol());
                 }
             }
@@ -142,11 +146,18 @@ public class TokenContract {
         public Double totalTokensSold(){
             double cont = 0d;
             for (PublicKey pKey : getBalances().keySet()){
-                if(!pKey.equals(owner)){
+                if(!pKey.equals(owner())){
                     cont += getBalances().get(pKey);
                 }
             }
             return cont;
+        }
+
+
+        public void payable(PublicKey pKey, Double cantidad){
+            if ((cantidad % COSTE) == 0){
+                transfer(pKey, cantidad / COSTE);
+                address.transferEZI(owner(), cantidad);}
         }
 
 }
